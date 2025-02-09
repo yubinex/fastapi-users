6c69528601e1cc605f0660a5aaa9ef13d5f1a83c
yubinex <janhauck99@gmail.com>
2025-02-09 14:04:37 +0100

    implement basic user registration endpoint

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import EmailStr
+from sqlmodel import SQLModel, Field, Session, create_engine, select
+from typing import Optional
+from contextlib import asynccontextmanager
 
 engine = create_engine("sqlite:///users.db")
-Base = declarative_base()
-
-app = FastAPI()
 
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    firstname = Column(String)
-    lastname = Column(String)
-    email = Column(String)
-    password = Column(String)
-    age = Column(Integer)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
-class UserModel(BaseModel):
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+    print("shutdown")
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+class UserModel(SQLModel):
     firstname: str
     lastname: str
     email: EmailStr
@@ -29,15 +29,23 @@ class UserModel(BaseModel):
     age: int
 
 
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
+class UserTable(UserModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
 
 
 @app.post("/register", status_code=201)
 async def create_user(user: UserModel):
-    new_user = User(**user.model_dump())
-    session.add(new_user)
-    session.commit()
-    return {"id": new_user.id, "message": "user registered successfully"}
+    new_user = UserTable.model_validate(user)
+    with Session(engine) as session:
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+        return {"id": new_user.id, "message": "user registered successfully"}
+
+
+@app.get("/users", status_code=200)
+async def get_users():
+    statement = select(UserTable)
+    with Session(engine) as session:
+        users = session.exec(statement).all()
+        return users
